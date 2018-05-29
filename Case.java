1e4b9f73e60242ab19da657544aa4b038d8a33ba
Mounir-A <39265893+Mounir-A@users.noreply.github.com>
2018-05-29 09:14:03 +0200

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,38 @@
+
+	package modele;
+
+	public class Case {
+		
+		private  int x;
+		private int y;
+		
+		public Case(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int getX() {
+			return this.x;
+		}
+
+		public int getY() {
+			return this.y;
+		}
+
+		public void setX(int x0) {
+			this.x=x0;
+		}
+
+		public void setY(int y0) {
+			this.y=y0;
+		}
+	
+		
+		
+		
+		
+		
+		
+	
+}
+
